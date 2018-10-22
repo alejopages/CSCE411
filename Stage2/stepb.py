8aac9143490a04843dccfd2b1f56d0b6bd740f9c
alpacapages <alpacapages@gmail.com>
2018-10-22 15:08:48 -0500

    Stage 2 started

--- Stage2/stepb.py
+++ Stage2/stepb.py
@@ -1,33 +1,80 @@
-import MySQLdb
+import mysql.connector
+import os
+from math import ceil
+
 
 def main():
-  max_files = 2000
 
-  try:
-    db = connect_db()
-  except Exception as e:
-    quit("Quitting")
+    # default constants
+    max_num_files = 2000
+    dat_dir = os.path.normpath('./data')
 
-  db.query("SHOW DATABASES")
+    try:
+        db = connect_db()
+    except:
+        quit()
 
-  return
+    cur = db.cursor()
+    count_cur = db.cursor()
+    table_cur = db.cursor()
 
+    cur.execute('SHOW TABLES')
+
+    for table in cur.fetchall():
+
+        print("Fetching from {}".format(table[0]))
+
+        count_cur.execute("SELECT COUNT(*) FROM " + table[0])
+        num_entries = count_cur.fetchall()[0][0]
+        num_entries_per_file = ceil(num_entries / max_num_files)
+
+        table_cur.execute("SELECT * FROM " + table[0])
+
+        i = 0
+        entries_per_file_counter = 0
+        dat_file = open(os.path.join(dat_dir, "{}_{:06d}.dat".format(table[0], 0)), 'wb')
+
+        for vals in table_cur.fetchall():
+
+            if entries_per_file_counter == num_entries_per_file:
+                dat_file.close()
+                dat_file = open(os.path.join(dat_dir, "{}_{:06d}.dat".format(table[0], i)), 'wb')
+                entries_per_file_counter = 0
+            else:
+                entries_per_file_counter += 1
+
+            dat = stringifyVals(vals)
+
+            dat_file.write(dat.encode())
+
+    db.close()
+    return
+
+
+def stringifyVals(vals):
+    string = ""
+    for val in vals:
+        string += " " + str(val) + " "
+    return string
 
 def connect_db():
-  try:
-      db = MySQLdb.connect(
-      host="cse.unl.edu",
-      user="apages",
-      passwd="Wtpt4R",
-      db="apages"
-    )
-  except Exception as e:
-    print("Could not connect to DB. {}".format(e))
-    raise e
-  return db
 
+    config = {
+        'user': 'cfarmer',
+        'password': 'eKd65T',
+        'host': 'cse.unl.edu',
+        'database': 'cfarmer'
+    }
 
+    try:
+        db = mysql.connector.connect(**config)
+    except Exception as e:
+        print('Could not connect to database: {}'.format(e))
+        raise e
+
+    return db
 
 
 if __name__ == '__main__':
-  main()
+
+    main()
