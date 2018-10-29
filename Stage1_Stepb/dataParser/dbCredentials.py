34caebb49c9bf3ab7b7f52ba4ab1a71c918519fc
Christian Farmer <christian.gt.farmer@gmail.com>
2018-10-28 20:10:51 -0500

    Finished Stage1_Stepb Parts 1-2

---
+++ Stage1_Stepb/dataParser/dbCredentials.py
@@ -0,0 +1,10 @@
+"""
+    The purpose of this file is to provide a reference to
+    the user's provided database credentials
+"""
+
+USER = 'cfarmer' # Ex: cfarmer
+PASSWORD = 'eKd65T' # Your DB password - not necessarily the
+                    # same as your CSE password
+HOST = 'cse.unl.edu' # Ex: cse.unl.edu
+DATABASE = 'cfarmer' # Ex: cfarmer
