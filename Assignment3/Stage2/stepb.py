b3ca1f0b4394df0ff6df03218775fea77fd3651d
alpacapages <alpacapages@gmail.com>
2018-10-29 18:54:39 -0500

    Trying still

--- Assignment3/Stage2/stepb.py
+++ Assignment3/Stage2/stepb.py
@@ -1,33 +1,16 @@
-import MySQLdb
+import mysql.connector
+import os
+from math import ceil
+import json
+import traceback
+import datetime
+import Stage2
+
 
 def main():
-  max_files = 2000
-
-  try:
-    db = connect_db()
-  except Exception as e:
-    quit("Quitting")
-
-  db.query("SHOW DATABASES")
-
-  return
-
-
-def connect_db():
-  try:
-      db = MySQLdb.connect(
-      host="cse.unl.edu",
-      user="apages",
-      passwd="Wtpt4R",
-      db="apages"
-    )
-  except Exception as e:
-    print("Could not connect to DB. {}".format(e))
-    raise e
-  return db
-
-
+	sb = Stage2.stage2()
+	sb.stepa()
 
 
 if __name__ == '__main__':
-  main()
+	main()
